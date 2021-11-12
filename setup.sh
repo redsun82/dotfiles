8d75097786ac3159c517ba6f164ce36ac342151a
Paolo Tranquilli <redsun82@github.com>
2021-11-12 11:05:50 +0100

    fix byobu-enable and add aliases

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get install -y bash-completion ack-grep tmux byobu
 
-byobu enable
+byobu-enable
 
 git config --global core.editor "code --wait"
 
@@ -11,3 +11,15 @@ git config --global alias.st status
 git config --global alias.cp cherry-pick
 git config --global alias.hist "log --oneline --decorate"
 git config --global alias.amend "commit --amend"
+
+cat >> ~/.bashrc << EOF
+
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias .......='cd ../../../../../..'
+alias -- -='cd -'
+EOF
+
