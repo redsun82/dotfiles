f55218940eebdfc4b836eedff00f7361a0c59bbf
Paolo Tranquilli <redsun82@github.com>
2021-11-15 15:24:29 +0100

    remove ssh stuff
    It's not working any way

--- setup.sh
+++ setup.sh
@@ -12,12 +12,6 @@ git config --global alias.cp cherry-pick
 git config --global alias.hist "log --oneline --decorate"
 git config --global alias.amend "commit --amend"
 
-if [[ -n "$SSH_KEY" ]]; then
-  mkdir -p ~/.ssh
-  echo $SSH_KEY > ~/.ssh/id_ed25519
-  chmod go-rw ~/.ssh/id_ed25519
-fi
-
 cat >> ~/.bashrc << EOF
 
 alias ..='cd ..'
@@ -28,6 +22,5 @@ alias ......='cd ../../../../..'
 alias .......='cd ../../../../../..'
 alias -- -='cd -'
 
-unset SSH_KEY
 EOF
 
