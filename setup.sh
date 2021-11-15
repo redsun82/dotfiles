93446af0ab1c3f0776db500a7dd438b49db7f377
Paolo Tranquilli <redsun82@github.com>
2021-11-15 14:47:45 +0100

    add ssh key from codespace secrets

--- setup.sh
+++ setup.sh
@@ -12,6 +12,11 @@ git config --global alias.cp cherry-pick
 git config --global alias.hist "log --oneline --decorate"
 git config --global alias.amend "commit --amend"
 
+if [[ -n "$SSH_KEY" ]]; then
+  echo $SSH_KEY > .ssh/id_ed25519
+  chmod go-rw .ssh/id_ed25519
+fi
+
 cat >> ~/.bashrc << EOF
 
 alias ..='cd ..'
@@ -21,5 +26,7 @@ alias .....='cd ../../../..'
 alias ......='cd ../../../../..'
 alias .......='cd ../../../../../..'
 alias -- -='cd -'
+
+unset SSH_KEY
 EOF
 
