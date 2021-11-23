678dcce14f97049b07fe2dbe45aa4323c1d962b8
Paolo Tranquilli <redsun82@github.com>
2021-11-23 11:29:28 +0000

    add ssh id from codespace secret

--- setup.sh
+++ setup.sh
@@ -12,6 +12,17 @@ git config --global alias.cp cherry-pick
 git config --global alias.hist "log --oneline --decorate"
 git config --global alias.amend "commit --amend"
 
+key_file=~/.ssh/id_ed25519
+if [[ ! -f $key_file ]]; then
+  mkdir -p ~/.ssh
+  echo -----BEGIN OPENSSH PRIVATE KEY----- >> $key_file
+  for l in $SSH_KEY; do
+    echo $l >> $key_file
+  done
+  echo -----END OPENSSH PRIVATE KEY----- >> $key_file
+  chmod go-rw $key_file
+fi
+
 sed -ni '/^# dotfiles/,$!p' ~/.bashrc
 cat >> ~/.bashrc << EOF
 # dotfiles
