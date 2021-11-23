4f09f0f903270793647a7eb8a9bbfe2e8f91284c
Paolo Tranquilli <redsun82@github.com>
2021-11-23 11:19:41 +0000

    add pre-commit hook for ql

--- setup.sh
+++ setup.sh
@@ -12,7 +12,9 @@ git config --global alias.cp cherry-pick
 git config --global alias.hist "log --oneline --decorate"
 git config --global alias.amend "commit --amend"
 
+sed -ni '/^# dotfiles/,$!p' ~/.bashrc
 cat >> ~/.bashrc << EOF
+# dotfiles
 
 alias ..='cd ..'
 alias ...='cd ../..'
@@ -22,8 +24,14 @@ alias ......='cd ../../../../..'
 alias .......='cd ../../../../../..'
 alias -- -='cd -'
 
-if [[ -d /workspaces/semmle-code ]]; then
-  export PATH=$PATH:/workspaces/semmle-code/target/intree/codeql
+semmle_top=/workspaces/semmle-code
+if [[ -d \$semmle_top ]]; then
+  export PATH=\$PATH:\$semmle_top/target/intree/codeql
+  ql_pre_commit=\$semmle_top/.git/modules/ql/hooks/pre-commit
+  if [[ ! -f \$ql_pre_commit ]]; then
+    sed 's=^exit =python3 config/sync-files.py \&\& exit =' \$semmle_top/ql/misc/scripts/pre-commit > \$ql_pre_commit
+    chmod +x \$ql_pre_commit
+  fi
 fi
 EOF
 
