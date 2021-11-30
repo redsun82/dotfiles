20b370670b86de7fd9889847c87800926a4deca7
Paolo Tranquilli <redsun82@github.com>
2021-11-30 14:45:35 +0100

    remove git url rewriter

--- setup.d/git-set-url.sh
+++
@@ -1,9 +0,0 @@
-find /workspaces -name .git | while read git; do
-  (
-    cd $(dirname $git)
-    git status &>/dev/null || exit 0
-    url=$(git remote get-url origin | sed 's=https://github.com/=git@github.com:=')
-    git remote set-url origin $url
-    echo rewritten $PWD url
-  )
-done
