ce1a10a7dde12d76225a4bbfd353aefcd9aadc0a
Paolo Tranquilli <redsun82@github.com>
2022-01-18 16:25:51 +0100

    add local /opt codeql to path

--- bashrc.d/codeql-path.sh
+++ bashrc.d/codeql-path.sh
@@ -1,5 +1,10 @@
-vs_codeql=/home/codespace/.vscode-remote/data/User/globalStorage/github.vscode-codeql
-distrib=$(ls $vs_codeql -t 2>/dev/null | head -1)
-if [[ -n $distrib ]]; then
-  export PATH=$PATH:$vs_codeql/$distrib/codeql
+local_codeql=/opt/$(ls /opt | grep -i codeql | sort -V | tail -1)
+if [[ -d "$local_codeql" ]]; then
+  export PATH=$PATH:$local_codeql
+else
+  vs_codeql=/home/codespace/.vscode-remote/data/User/globalStorage/github.vscode-codeql
+  distrib=$(ls $vs_codeql -t 2>/dev/null | head -1)
+  if [[ -n $distrib ]]; then
+    export PATH=$PATH:$vs_codeql/$distrib/codeql
+  fi
 fi
