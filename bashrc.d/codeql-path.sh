492995ac52ecededecd4764f5b6a03197fa6d5e3
Paolo Tranquilli <redsun82@github.com>
2022-06-20 14:11:39 +0200

    fix auto-detected codeql

--- bashrc.d/codeql-path.sh
+++ bashrc.d/codeql-path.sh
@@ -1,10 +1,10 @@
 local_codeql=/opt/$(ls /opt | grep -i codeql | sort -V | tail -1)
-if [[ -d "$local_codeql" ]]; then
+if [[ "$local_codeql" != "/opt/" ]]; then
   export PATH=$PATH:$local_codeql
 else
   vs_codeql=$HOME/.vscode-remote/data/User/globalStorage/github.vscode-codeql
-  distrib=$(ls $vs_codeql -t 2>/dev/null | head -1)
-  if [[ -n $distrib ]]; then
-    export PATH=$PATH:$vs_codeql/$distrib/codeql
+  bin=$(ls $vs_codeql/*/codeql/codeql -t 2>/dev/null | head -1)
+  if [[ -n $bin ]]; then
+    export PATH=$PATH:$(dirname "$bin")
   fi
 fi
