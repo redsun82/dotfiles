2daab22c8eaecb746a018e9b9faf055fd09e7184
Paolo Tranquilli <redsun82@github.com>
2022-06-20 09:14:55 +0200

    remove obsolete pre-commit setup

--- bashrc.d/semmle-code-workspace.sh
+++
@@ -1,8 +0,0 @@
-if [[ $CODESPACE_VSCODE_FOLDER == /workspaces/semmle-code ]]; then
-  export PATH=$PATH:$CODESPACE_VSCODE_FOLDER/target/intree/codeql
-  ql_pre_commit=$CODESPACE_VSCODE_FOLDER/.git/modules/ql/hooks/pre-commit
-  if [[ ! -f $ql_pre_commit ]]; then
-    sed 's=^exit =python3 config/sync-files.py \&\& exit =' $CODESPACE_VSCODE_FOLDER/ql/misc/scripts/pre-commit > $ql_pre_commit
-    chmod +x $ql_pre_commit
-  fi
-fi
