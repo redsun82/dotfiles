d02f060e28cc99ec49096fce6b18a3f6210967b1
Paolo Tranquilli <redsun82@github.com>
2022-06-20 14:01:32 +0200

    fix hardcoded home path

--- setup.d/git.sh
+++ setup.d/git.sh
@@ -5,3 +5,5 @@ git config --global alias.st status
 git config --global alias.cp cherry-pick
 git config --global alias.hist "log --oneline --decorate"
 git config --global alias.amend "commit --amend"
+git config --global alias.ch checkout
+git config --global alias.su "submodule update --init --recursive"
